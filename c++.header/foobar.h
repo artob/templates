dc6c4f111d72ea740bf37af7568601c32ca3d166
Arto Bendiken <arto@bendiken.net>
2015-04-08 01:49:30 +0200

    Imported a template for single-header C++14 projects.

---
+++ c++.header/foobar.h
@@ -0,0 +1,16 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef FOOBAR_H
+#define FOOBAR_H
+
+/**
+ * <foobar.h>
+ */
+
+#ifndef __cplusplus
+#error "<foobar.h> requires a C++ compiler"
+#endif
+
+namespace foobar {}
+
+#endif /* FOOBAR_H */
