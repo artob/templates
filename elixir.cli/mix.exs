886968ff052a031bdd16ad15dbae9a464a485f9b
Arto Bendiken <arto@bendiken.net>
2016-08-06 00:19:32 +0200

    Copied the Elixir template as a basis for CLI projects.

---
+++ elixir.cli/mix.exs
@@ -0,0 +1,63 @@
+defmodule Foobar.Mixfile do
+  use Mix.Project
+
+  @name      "Foobar"
+  @version   File.read!("VERSION") |> String.strip
+  @github    "https://github.com/bendiken/foobar"
+  @bitbucket "https://bitbucket.org/bendiken/foobar"
+  @homepage  @github
+
+  def project do
+    [app: :foobar,
+     version: @version,
+     elixir: "~> 1.3",
+     compilers: Mix.compilers,
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     name: @name,
+     source_url: @github,
+     homepage_url: @homepage,
+     description: description(),
+     aliases: aliases(),
+     deps: deps(),
+     package: package(),
+     docs: [source_ref: @version, main: "readme", extras: ["README.md"]],
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: [
+       "coveralls": :test,
+       "coveralls.detail": :test,
+       "coveralls.post": :test,
+       "coveralls.html": :test,
+     ]]
+  end
+
+  def application do
+    [#mod: {Foobar, []},
+     applications: [:logger]]
+  end
+
+  defp package do
+    [files: ~w(lib priv src mix.exs CHANGES README.md UNLICENSE VERSION),
+     maintainers: ["Arto Bendiken"],
+     licenses: ["Public Domain"],
+     links: %{"GitHub" => @github, "Bitbucket" => @bitbucket}]
+  end
+
+  defp description do
+    """
+    Foobar is a template for Elixir projects.
+    """
+  end
+
+  defp deps do
+    [{:credo,       ">= 0.0.0", only: [:dev, :test]},
+     {:dialyxir,    ">= 0.0.0", only: [:dev, :test]},
+     {:earmark,     ">= 0.0.0", only: :dev},
+     {:ex_doc,      ">= 0.0.0", only: :dev},
+     {:excoveralls, ">= 0.0.0", only: :test}]
+  end
+
+  defp aliases do
+    []
+  end
+end
---
+++ elixir.cli/test/foobar_test.exs
@@ -0,0 +1,7 @@
+# This is free and unencumbered software released into the public domain.
+
+defmodule FoobarTest do
+  use ExUnit.Case, async: true
+
+  doctest Foobar
+end
