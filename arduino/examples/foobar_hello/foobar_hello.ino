740b6853fcdc83a44d603566df554ec6a1afcb96
Arto Bendiken <arto@bendiken.net>
2016-10-30 21:43:58 +0100

    Stubbed out an Arduino example.

---
+++ arduino/examples/foobar_hello/foobar_hello.ino
@@ -0,0 +1,12 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include <Arduino.h>
+#include <Foobar.h>
+
+void setup() {
+  // TODO
+}
+
+void loop() {
+  // TODO
+}
