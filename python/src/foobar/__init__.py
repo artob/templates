15c138f5061dccff0af9c653f028cd0336c6054e
Arto Bendiken <arto@bendiken.net>
2018-10-09 16:42:14 +0300

    Enforced the minimum required Python version.

--- python/src/foobar/__init__.py
+++ python/src/foobar/__init__.py
@@ -1,3 +1,10 @@
 # This is free and unencumbered software released into the public domain.
 
+"""Foobar."""
+
+import sys
+
+assert sys.version_info >= (3,5), \
+    "Foobar requires Python 3.5+"
+
 __all__ = []
