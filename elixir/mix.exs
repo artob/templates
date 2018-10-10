8451d1bd5f55345690e1a0961ee35411bf620eee
Arto Bendiken <arto@bendiken.net>
2018-10-10 12:34:01 +0300

    Added Elixir reference links to the Mixfile.

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -9,42 +9,48 @@ defmodule Foobar.MixProject do
 
   def project do
     [
+      # See: https://hexdocs.pm/mix/Mix.Project.html
       app: :foobar,
       version: @version,
-      elixir: "~> 1.7",
-      #compilers: Mix.compilers(),
-      #build_embedded: Mix.env() == :prod,
-      start_permanent: Mix.env() == :prod,
-      #name: @name,
-      #source_url: @github,
-      #homepage_url: @homepage,
-      #description: description(),
-      #aliases: aliases(),
       deps: deps(),
-      #package: package(),
-      #docs: [source_ref: @version, main: "readme", extras: ["README.md"]],
-      #test_coverage: [tool: ExCoveralls],
-      #preferred_cli_env: [
-      #  coveralls: :test,
-      #  "coveralls.detail": :test,
-      #  "coveralls.post": :test,
-      #  "coveralls.html": :test
-      #]
+      aliases: aliases(),
+
+      # See: https://hexdocs.pm/mix/Mix.Tasks.Compile.html
+      compilers: Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
+
+      # See: https://hexdocs.pm/mix/Mix.Tasks.App.Start.html
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+
+      # See: https://hexdocs.pm/ex_doc/Mix.Tasks.Docs.html
+      name: @name,
+      source_url: @github,
+      homepage_url: @homepage,
+      docs: [
+        source_ref: @version,
+        main: "readme",
+        extras: ["README.md", "CHANGES.md"]
+      ],
+      description: description(),
+
+      # See: https://github.com/parroty/excoveralls
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+
+      # See: https://hex.pm/docs/publish
+      package: package()
     ]
   end
 
   def application do
     [
       extra_applications: [:logger]
-    ]
-  end
-
-  defp package do
-    [
-      files: ~w(lib priv src mix.exs CHANGES.md README.md UNLICENSE VERSION),
-      maintainers: ["Arto Bendiken"],
-      licenses: ["Public Domain"],
-      links: %{"GitHub" => @github, "Bitbucket" => @bitbucket}
     ]
   end
 
@@ -61,11 +67,21 @@ defmodule Foobar.MixProject do
       {:dialyxir, "~> 0.5", only: [:dev, :test]},
       {:earmark, "~> 1.2", only: :dev},
       {:ex_doc, "~> 0.19", only: :dev},
-      {:excoveralls, "~> 0.10", only: :test},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
   defp aliases do
     []
+  end
+
+  defp package do
+    # See: https://hex.pm/docs/publish
+    [
+      files: ~w(lib priv src mix.exs CHANGES.md README.md UNLICENSE VERSION),
+      maintainers: ["Arto Bendiken"],
+      licenses: ["Public Domain"],
+      links: %{"GitHub" => @github, "Bitbucket" => @bitbucket}
+    ]
   end
 end
